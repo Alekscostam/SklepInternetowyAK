a451380c2ffd7e9414798e37507825bd65255332
Alekscostam <kowalski121.al@gmail.com>
2021-03-23 10:29:12 +0100

    Adding classes and context

---
+++ SklepInternetowyAK/DAL/FilmsContext.cs
@@ -0,0 +1,22 @@
+﻿using SklepInternetowyAK.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SklepInternetowyAK.DAL
+{
+    public class FilmsContext : DbContext
+    {
+
+        public DbSet<Film> Films { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public FilmsContext() : base("FilmsContext")
+        {
+
+        }
+    }
+}
